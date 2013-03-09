bec2f6a953b450ab94792040b7dad4ca375da600
Rebill <rebill@qq.com>
2013-03-09 22:36:53 +0800

    add useage and document

--- storm.go
+++ storm.go
@@ -1,13 +1,24 @@
 package main
 
-import "flag"
-import "fmt"
-import "net/http"
-import "runtime"
+import (
+    "flag"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "runtime"
+    "strings"
+    "time"
+)
+
+const VERSION = "0.1.0"
 
 var (
+	help = flag.Bool("h", false, "Helps")
 	client = flag.Int("c", 10, "Clients")
-	seconds = flag.Int("t", 60, "Seconds")
+	seconds = flag.Int64("t", 60, "Seconds")
+	url = flag.String("url", "", "URL")
+	file = flag.String("f", "", "URL list file")
+	urls []string
 )
 
 func fetch(url string, c chan bool) {
@@ -24,14 +35,69 @@ func fetch(url string, c chan bool) {
 	c <- status
 }
 
+func readLines(filename string)(lines []string, err error) {
+    bytes, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return
+    }
+    
+    for _, line := range strings.Split(string(bytes), "\n") {
+        if line != "" {             
+            lines = append(lines, line)
+        }
+    }
+    return
+}
+
+func usage() {
+	fmt.Println("Storm version:", VERSION)
+
+	fmt.Println("Usage: storm [-c clients] [-t Seconds] [-url url]")
+	fmt.Println("")
+	fmt.Println("Options:")
+	fmt.Println("-h\t:", "this help")
+	fmt.Println("-c\t:", "how many clients you want to simulate")
+	fmt.Println("-t\t:", "how long you want to bench")
+	fmt.Println("-url\t:", "bench url")
+	fmt.Println("-f\t:", "url list file, one url per line.")
+}
+
 func main() {
 	flag.Parse()
+	if *help {
+		usage()
+		return
+	}
+	filename := *file
+	if (*url == "" && filename == "") {
+	    fmt.Println("Please input url.")
+	    return
+	}
+	
+	if filename != "" {
+	    lines, err := readLines(filename)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        urls = lines
+	}
 	fmt.Println(*client, "clients, run", *seconds, "seconds")
 	runtime.GOMAXPROCS(*client)
+	
 	success, fail := 0, 0
 	c := make(chan bool)
-	for i := 0; i < 3; i++ {
-		go fetch("http://www.baidu.com/", c)
+	
+	st := time.Now().Unix()
+	for (st + *seconds > time.Now().Unix()) {
+	    if filename != "" {
+	        for _, uri := range urls {
+                go fetch(uri, c)
+            }
+	    } else {
+	        go fetch(*url, c)
+	    }	    
+		
 		status := <- c
 		if status == true {
 			success += 1
